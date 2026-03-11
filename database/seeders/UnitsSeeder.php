027c85db1cc1fc7ef694d54f2a546c422760e550
Đỗ Tiến Chí <chidt@unifiedx.co.jp>
2026-03-11 08:38:52 +0700

    add unit seeder

---
+++ database/seeders/UnitsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UnitsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $units = [
+            ['name' => 'Chiếc', 'unit' => 'chiếc'],
+            ['name' => 'Bộ', 'unit' => 'bộ'],
+            ['name' => 'Đôi', 'unit' => 'đôi'],
+            ['name' => 'Cái', 'unit' => 'cái'],
+            ['name' => 'Hộp', 'unit' => 'hộp'],
+            ['name' => 'Con', 'unit' => 'con'],
+            ['name' => 'Set', 'unit' => 'set'],
+            ['name' => 'Combo', 'unit' => 'combo'],
+        ];
+        foreach ($units as $unit) {
+            \App\Models\Unit::create($unit);
+        }
+    }
+}
